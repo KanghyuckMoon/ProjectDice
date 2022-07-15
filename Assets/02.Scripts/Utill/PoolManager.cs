6f48762e14aa6b4977798fb3ca4e7f014582d6c4
bossxz <76054436+bossxz@users.noreply.github.com>
2022-07-16 04:09:59 +0900

    07/16_유니티 버전업

---
+++ Assets/02.Scripts/Utill/PoolManager.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PoolManager : Singleton<PoolManager>
+{
+	//풀링
+	public Dictionary<string, object> queueDictionary = new Dictionary<string, object>();
+
+	private void Start()
+	{
+		SceneManager.sceneLoaded += () => ClearQueue();
+	}
+
+	/// <summary>
+	/// 큐 제거
+	/// </summary>
+	private void ClearQueue()
+	{
+		queueDictionary.Clear();
+	}
+
+	/// <summary>
+	/// 큐에서 오브젝트 가져오기
+	/// </summary>
+	public T GetObject<T>()
+	{
+		if (queueDictionary.TryGetValue(typeof(T).Name, out var something))
+		{
+			var queue = something as Queue<T>;
+			if (queue.Count > 0)
+			{
+				return queue.Dequeue();
+			}
+		}
+		return default(T);
+	}
+
+	/// <summary>
+	/// 큐에 오브젝트 등록하기
+	/// </summary>
+	public void RegisterObject<T>(T obj)
+	{
+		if (queueDictionary.TryGetValue(typeof(T).Name, out var something))
+		{
+			var queue = something as Queue<T>;
+			queue.Enqueue(obj);
+		}
+		else
+		{
+			AddQueue<T>();
+			var queue = queueDictionary[typeof(T).Name] as Queue<T>;
+			queue.Enqueue(obj);
+		}
+	}
+
+	/// <summary>
+	/// 큐 등록
+	/// </summary>
+	/// <typeparam name=""></typeparam>
+	private void AddQueue<T>()
+	{
+		Queue<T> queue = new Queue<T>();
+		if (!queueDictionary.TryGetValue(typeof(T).Name, out var something))
+		{
+			queueDictionary.Add(typeof(T).Name, queue);
+		}
+	}
+
+}
---
+++ Assets/02.Scripts/Utill/Singleton.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+	private static T _instance; //속성으로 가질 인스턴스, 전역으로 선언하여 전역적인 접근점을 제공한다
+	public static T Instance //인스턴스를 반환하는 프로퍼티
+	{
+		get
+		{
+			//이미 만들어진 인스턴스가 없으면
+			if (_instance == null)
+			{
+				//인스턴스가 있는지 탐색
+				_instance = FindObjectOfType<T>();
+
+				//그래도 없으면 새로 만든다
+				if (_instance == null)
+				{
+					_instance = new GameObject(typeof(T).Name).AddComponent<T>();
+				}
+			}
+
+			return _instance;
+		}
+	}
+
+	public void Awake()
+	{
+		//만약 인스턴스가 없을시
+		if (_instance == null)
+		{
+			//인스턴스는 해당 오브젝트가 된다.
+			_instance = this as T;
+
+			//그리고 해당 오브젝트는 삭제되지 않는다.
+			DontDestroyOnLoad(gameObject);
+		}
+		else
+		{
+			//이미 인스턴스가 존재할 경우 이 오브젝트는 삭제된다.
+			Destroy(gameObject);
+		}
+	}
+
+}
