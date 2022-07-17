e81e8bff8ee6690b0cc4acedb421b11bc99a7281
moonkanghyuck <moonkanghyuck@gmail.com>
2022-07-18 00:29:29 +0900

    07/18_방향키 대응

--- Assets/02.Scripts/InputManager.cs
+++ Assets/02.Scripts/InputManager.cs
@@ -51,30 +51,30 @@ public class InputManager : MonoBehaviour
 
 		if(_isRollMode)
 		{
-			if (Input.GetKey(KeyCode.D))
+			if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
 			{
 				Roll(90);
 			}
-			if (Input.GetKey(KeyCode.A))
+			if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
 			{
 				Roll(-90);
 			}
 		}
 		else
 		{
-			if (Input.GetKey(KeyCode.W))
+			if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
 			{
 				Move(0, 1);
 			}
-			if (Input.GetKey(KeyCode.S))
+			if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
 			{
 				Move(0, -1);
 			}
-			if (Input.GetKey(KeyCode.D))
+			if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
 			{
 				Move(1, 0);
 			}
-			if (Input.GetKey(KeyCode.A))
+			if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
 			{
 				Move(-1, 0);
 			}
